8a6d81b57ea196cbf5bbec350a26f3670ca97aa1
infoarifk <inbox.arifkhan@gmail.com>
2023-11-12 01:45:53 +0600

    updated

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//pestsolution
-//h39MbaHwhKLLRnwP
-
-
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.nud4ico.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,12 +20,28 @@ const client = new MongoClient(uri, {
   }
 });
 
+const database = client.db("pestSolutionDB");
+const contactCollection = database.collection("contacts");
+
 async function run() {
   try {
     
     await client.connect();
 
-    //code here
+    
+    app.post("/contacts", async(req, res)=>{
+
+
+        const contact = req.body;
+        console.log("received users: ", contact);
+       
+        //send data to database
+        const result = await contactCollection.insertOne(contact);
+        res.send(result);
+  
+  
+      });
+  
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
